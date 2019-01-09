d14f04e653e4e155286cffbc32b08fa4c85b273d
semvs <semvs@ayvansaray.edu.tr>
2019-01-09 09:08:25 +0300

    BaglantiYapildi

--- Baglanti/Form1.Designer.cs
+++ Baglanti/Form1.Designer.cs
@@ -38,8 +38,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(89, 51);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Aç";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -47,8 +48,9 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(93, 51);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Kapat";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -59,6 +61,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- Baglanti/Form1.cs
+++ Baglanti/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,30 @@ namespace Baglanti
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlServer"].ConnectionString);
+       // DataSet ds = new DataSet();
+       // DataTable dt;
+       // SqlDataAdapter adap = new SqlDataAdapter();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            MessageBox.Show("Connection Acıldı.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            MessageBox.Show("Connection Kapandı.");
         }
     }
 }
